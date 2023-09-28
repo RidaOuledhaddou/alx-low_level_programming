2f358963ca4a4454bf4c4e0be3a1f96a80e975b9
RidaOuledhaddou <ridaouledhaddo@gmail.com>
2023-09-28 06:28:29 -0700

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,14 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s + 1);
+		_puts_recursion(s + 1);
 		_putchar(*s);
+	}else{
+		_putchar(\n);
 	}
 }
 
