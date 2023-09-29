69c96d0464a988bed3306ee3155ed76b52433073
RidaOuledhaddou <ridaouledhaddo@gmail.com>
2023-09-29 10:07:54 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,17 +4,15 @@ int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n : number to calculate
+ * @n: number to calculate the square root of
  *
  * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-	return (-1);
-}
-return (actual_sqrt_recursion(n, 0));
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
@@ -32,3 +30,5 @@ int actual_sqrt_recursion(int n, int i)
 	if (i * i == n)
 		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
+}
+
